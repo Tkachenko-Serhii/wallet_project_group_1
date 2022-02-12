880e6a83bfdffb1644926dc1bc6ee546f2dfa9bb
Serhii Tkachenko <79119130+Tkachenko-Serhii@users.noreply.github.com>
2022-02-12 20:58:00 +0200

    Update

--- src/App.js
+++ src/App.js
@@ -1,16 +1,16 @@
-import './App.css';
-import LoginPage from './pages/LoginPage';
-import RegisterPage from './pages/RegisterPage';
-import { Routes, Route, Navigate } from 'react-router-dom';
+import "./App.css";
+import LoginPage from "./pages/LoginPage";
+import RegisterPage from "./pages/RegisterPage";
+import { Routes, Route, Navigate } from "react-router-dom";
 
 function App() {
   return (
-    <div className="App">
+    <div className='App'>
       <Routes>
-        <Route path="/" element={<Navigate to="/login" />} />
-        <Route path="/login" element={<LoginPage />} />
-        <Route path="/register" element={<RegisterPage />} />
-        <Route path="*" element={<Navigate to="/" />} />
+        <Route path='/' element={<Navigate to='/login' />} />
+        <Route path='/login' element={<LoginPage />} />
+        <Route path='/register' element={<RegisterPage />} />
+        <Route path='*' element={<Navigate to='/' />} />
       </Routes>
     </div>
   );
