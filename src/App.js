1e7cf7b5a30d7a5a00561be07fc47485ff4810e3
Olgashulz <41305602+Olgashulz@users.noreply.github.com>
2022-02-12 10:54:23 +0200

    bg + container +buttons

--- src/App.js
+++ src/App.js
@@ -1,7 +1,14 @@
 import "./App.css";
+import Container from "./components/Container";
+import Button from "./components/Button";
 
 function App() {
-  return <div className='App'></div>;
+  return <div className='App'>
+    <Container>
+      <Button text="Добавить" color="green" />
+      <Button text="Отмена" color="white" />
+    </Container>
+  </div>;
 }
 
 export default App;
