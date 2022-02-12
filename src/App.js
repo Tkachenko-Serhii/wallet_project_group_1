841585f9700ac200426239148727efd66d512abb
Olgashulz <41305602+Olgashulz@users.noreply.github.com>
2022-02-12 20:03:49 +0200

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,17 +1,19 @@
-import "./App.css";
-import Container from "./components/Container";
-import Button from "./components/Button";
-import FormAddTransaction from "./components/FormAddTransaction"
-
+import './App.css';
+import LoginPage from './pages/LoginPage';
+import RegisterPage from './pages/RegisterPage';
+import { Routes, Route, Navigate } from 'react-router-dom';
 
 function App() {
-  return <div className='App'>
-    <Container>
-      <Button text="Добавить" color="green" />
-      <Button text="Отмена" color="white" />
-      <FormAddTransaction />
-    </Container>
-  </div>;
+  return (
+    <div className="App">
+      <Routes>
+        <Route path="/" element={<Navigate to="/login" />} />
+        <Route path="/login" element={<LoginPage />} />
+        <Route path="/register" element={<RegisterPage />} />
+        <Route path="*" element={<Navigate to="/" />} />
+      </Routes>
+    </div>
+  );
 }
 
 export default App;
