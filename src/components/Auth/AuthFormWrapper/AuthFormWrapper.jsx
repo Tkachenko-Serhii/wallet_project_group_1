f8baddb99ef40818ccee1f2a217bccfe5cd4f442
Roman Kruk <wilmlass@gmail.com>
2022-02-14 20:36:46 +0200

    added prop types to auth pages components, added animations to auth forms and auth media container

--- src/components/Auth/AuthFormWrapper/AuthFormWrapper.jsx
+++ src/components/Auth/AuthFormWrapper/AuthFormWrapper.jsx
@@ -1,3 +1,4 @@
+import { Fade } from '@mui/material';
 import styles from './AuthFormWrapper.module.css';
 
 import Logo from '../../Logo';
@@ -5,10 +6,12 @@ import Logo from '../../Logo';
 export default function AuthFormWrapper({ children }) {
 	return (
 		<div className={styles.blurBackgroundWrapper}>
-			<div className={styles.formWrapper}>
-				<Logo />
-				{children}
-			</div>
+			<Fade in={true} timeout={500} mountOnEnter unmountOnExit>
+				<div className={styles.formWrapper}>
+					<Logo />
+					{children}
+				</div>
+			</Fade>
 		</div>
 	);
 }
