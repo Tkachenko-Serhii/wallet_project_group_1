998230e01658d88c5962e6775082bfc88fdacd62
Snizhana-Mykhaylova <snejana.pallada@gmail.com>
2022-02-15 17:52:47 +0100

    add currency component

--- src/pages/LoginPage/LoginPage.jsx
+++ src/pages/LoginPage/LoginPage.jsx
@@ -7,12 +7,12 @@ import LoginForm from '../../components/Auth/LoginForm';
 import styles from './LoginPage.module.css';
 
 export default function LoginPage() {
-	const matches = useMediaQuery('(min-width:768px)');
+  const matches = useMediaQuery('(min-width:768px)');
 
-	return (
-		<AuthPageWrapper>
-			{matches && <AuthMedia className={styles.bgImage} />}
-			<LoginForm />
-		</AuthPageWrapper>
-	);
+  return (
+    <AuthPageWrapper>
+      {matches && <AuthMedia className={styles.bgImage} />}
+      <LoginForm />
+    </AuthPageWrapper>
+  );
 }
