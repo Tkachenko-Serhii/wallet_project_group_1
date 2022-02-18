bab6ee44d76453a3eb184351f298898bdb60888f
Olgashulz <41305602+Olgashulz@users.noreply.github.com>
2022-02-18 15:31:14 +0200

    форма

--- src/components/FormAddTransaction/FormAddTransaction.js
+++ src/components/FormAddTransaction/FormAddTransaction.js
@@ -8,7 +8,6 @@ import "react-datetime/css/react-datetime.css";
 import showModal from '../../redux/modal/modalActions';
 import addTransaction from "../../redux/form/formOperations";
 
-
 import { ReactComponent as Plus } from '../../icons/plus.svg'
 import { ReactComponent as Minus } from '../../icons/minus.svg'
 import { ReactComponent as CalendarIcon } from '../../icons/calendar.svg'
@@ -18,12 +17,10 @@ import Button from '../Button';
 import DatePicker from '../DatePicker';
 import styles from './FormAddTransaction.module.css';
 
-// Modal.setAppElement('#root')
-
 export default function ModalAddTransaction(props) {
 
     const defaultTransactionState = {
-        type: true,
+        type: false,
         sum: '',
         date: new Date(),
         comment: '',
@@ -32,27 +29,20 @@ export default function ModalAddTransaction(props) {
 
     const [transaction, setTransaction] = useState(defaultTransactionState);
     const dispatch = useDispatch();
-    // const [modalIsOpen, setIsOpen] = useState(false)
-
 
     const categories = [
-        { value: "Main", label: "Вasic costs" },
-        { value: "Food", label: "Еда" },
-        { value: "Car", label: "Авто" },
-        { value: "Development", label: "Развитие" },
-        { value: "Children", label: "Дети" },
-        { value: "Home", label: "Дом" },
-        { value: "Education", label: "Образование" },
-        { value: "Other", label: "Остальные" },
+        { value: "Main", label: "Вasic costs", type: false },
+        { value: "Food", label: "Food", type: false },
+        { value: "Car", label: "Авто", type: false },
+        { value: "Development", label: "Развитие", type: false },
+        { value: "Children", label: "Дети", type: false },
+        { value: "Home", label: "Дом", type: false },
+        { value: "Education", label: "Образование", type: false },
+        { value: "Other", label: "Остальные", type: false },
+        { value: "Regular income", label: "Regular income", type: true },
+        { value: "Additional income", label: "Additional income", type: true },
     ]
 
-    // function openModal() {
-    //     setIsOpen(true)
-    // }
-
-    // function closeModal() {
-    //     setIsOpen(false)
-    // }
 
     const handleInputChange = event => {
         const { name, value } = event.currentTarget;
@@ -65,7 +55,6 @@ export default function ModalAddTransaction(props) {
                 else {
                     updateTransaction(name, false)
                 }
-
                 break;
 
             case 'sum':
@@ -88,6 +77,7 @@ export default function ModalAddTransaction(props) {
     const handleSubmit = (event) => {
         event.preventDefault();
         const newTransaction = {
+            ...transaction,
             type: transaction.type,
             sum: transaction.sum,
             date: transaction.date.toLocaleDateString(),
@@ -106,104 +96,95 @@ export default function ModalAddTransaction(props) {
     const resetForm = (event) => {
         event.preventDefault();
         setTransaction(defaultTransactionState)
-        // closeModal()
     }
 
 
     return (
+        <form
+            onSubmit={handleSubmit}
+            className={styles.form} >
 
-        <>
-            {/* <button className={styles.openButton} onClick={openModal}>
-                <Plus />
+            <button className={styles.closeButton} onClick={(event) => dispatch(showModal())}>
+                <Close />
             </button>
-            <Modal
-                isOpen={modalIsOpen}
-                onRequestClose={closeModal}
-                overlayClassName={styles.modalOverlay}
-                contentLabel="Example Modal"
-                className={styles.modalContainer}
-                htmlOpenClassName="no-scroll"
-            > */}
-
-            <form
-                onSubmit={handleSubmit}
-                className={styles.form} >
-
-                <button className={styles.closeButton} onClick={(event) => dispatch(showModal())}>
-                    <Close />
-                </button>
-                <h4 className={styles.title}>Add transaction</h4>
+            <h4 className={styles.title}>Add transaction</h4>
 
 
-                <div className={styles.swichContainer}>
-                    <span className={styles.swichTitle}>Expense</span>
+            <div className={styles.swichContainer}>
+                <span className={styles.swichTitle}>Income</span>
 
-                    <label className={styles.switch}>
-                        <input
-                            type="checkbox"
-                            name="type"
-                            onChange={handleInputChange}
-                            checked={!transaction.type}
-                        />
-                        <div className={styles.slider}>
-                            <div className={styles.indicator}>
-                                {transaction.type ? <Plus /> : <Minus />}
-                            </div>
-                        </div>
-                    </label>
-                    <span className={styles.swichTitle}>Income</span>
-                </div>
-
-
-                {!transaction.type ? <div className={styles.categoriesContainer}>
-                    <Select
-                        placeholder="Select a category "
-                        options={categories}
-                        styles={stylesSelect()}
-                        onChange={(option) => {
-                            updateTransaction('category', option.value)
-                        }} />
-                </div>
-                    : null
-                }
-
-                <div className={styles.amountAndDateContainer}>
-                    <div className={styles.dateContainer}>
-                        <label className={styles.labelForm}>
-                            <input
-                                type="text"
-                                name="sum"
-                                placeholder='0.00'
-                                className={styles.inputForm}
-                                onChange={handleInputChange}
-                                required />
-                        </label>
-                    </div>
-
-                    <div className={styles.datepickerContainer}>
-                        <DatePicker date={transaction.date} updateDate={updateTransaction} />
-                        <CalendarIcon className={styles.calendarIcon} />
-                    </div>
-
-                </div>
-
-                <label className={styles.labelForm}>
+                <label className={styles.switch}>
                     <input
-                        type="text"
-                        name="comment"
-                        placeholder='Comment'
-                        className={styles.inputComment}
+                        type="checkbox"
+                        name="type"
                         onChange={handleInputChange}
-                        required />
+                        checked={!transaction.type}
+                    />
+                    <div className={styles.slider}>
+                        <div className={styles.indicator}>
+                            {transaction.type ? <Plus /> : <Minus />}
+                        </div>
+                    </div>
                 </label>
-                <Button type="submit" text="Add" color="green" />
-                <Button
-                    text="Cancel"
-                    color="white"
-                    onClick={resetForm} />
-            </form >
-            {/* </Modal> */}
-        </>
+                <span className={styles.swichTitle}>Expense</span>
+            </div>
+
+
+            <div className={styles.categoriesContainer}>
+                <Select
+                    placeholder="Select a category "
+                    options={categories.filter(category => category.type === transaction.type)}
+                    styles={stylesSelect()}
+                    onChange={(option) => {
+                        updateTransaction('category', option.value)
+                    }} />
+            </div>
+
+            <div className={styles.amountAndDateContainer}>
+                <div className={styles.dateContainer}>
+                    <label className={styles.labelForm}>
+                        <input
+                            type="number"
+                            name="sum"
+                            placeholder='0.00'
+                            className={styles.inputForm}
+
+                            onChange={(event) => {
+                                if (
+                                    event.target.value === "" ||
+                                    /^[0-9]*(\.[0-9]?[0-9]?)?$/.test(event.target.value)
+                                ) {
+                                    handleInputChange(event)
+                                }
+                            }}
+                            required
+                        />
+                    </label>
+                </div>
+
+                <div className={styles.datepickerContainer}>
+                    <DatePicker date={transaction.date} updateDate={updateTransaction} />
+                    <CalendarIcon className={styles.calendarIcon} />
+                </div>
+
+            </div>
+
+            <label className={styles.labelForm}>
+                <input
+                    type="text"
+                    name="comment"
+                    placeholder='Comment'
+                    className={styles.inputComment}
+                    onChange={handleInputChange}
+                    required />
+            </label>
+            <Button type="submit" text="Add" color="green" />
+            <Button
+                text="Cancel"
+                color="white"
+                onClick={resetForm}
+            />
+        </form >
     )
 }
 
