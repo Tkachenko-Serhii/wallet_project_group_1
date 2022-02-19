f70bc74af6319038da38ca9b9d6886802bb7a758
Olgashulz <41305602+Olgashulz@users.noreply.github.com>
2022-02-19 22:07:36 +0200

    стили + добавление транзакции

--- src/utils/index.js
+++ src/utils/index.js
@@ -3,3 +3,4 @@ export { default as registerFormValidationSchema } from './registerFormValidatio
 export { default as authErrorHandler } from './AuthErrorHandler';
 export { default as passwordAttention } from './passwordAttention';
 export { default as authSuccessNotification } from './authSuccessNotification';
+export { default as formAddTransactionSchema } from './formAddValidation';
