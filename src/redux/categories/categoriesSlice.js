fbf199b6c60a4d129d10e43b40d427e43cf021bc
Tetiana Hraborova <77548827+Lera24@users.noreply.github.com>
2022-02-21 22:48:50 +0200

    fix-spinner-and-logout-modal

--- src/redux/categories/categoriesSlice.js
+++ src/redux/categories/categoriesSlice.js
@@ -16,18 +16,15 @@ const categoriesSlice = createSlice({
   extraReducers: {
     [categoriesOperations.getCategories.fulfilled](state, { payload }) {
       state.all = [...payload];
-      state.isLoading = false;
     },
     [categoriesOperations.getCategories.rejected](state, { payload }) {
       state.serverError = {
         status: payload.status,
         message: payload.message,
       };
-      state.isLoading = false;
     },
 
     [categoriesOperations.getCategories.pending](state, { payload }) {
-      state.isLoading = true;
       state.serverError = {
         status: null,
         message: null,
