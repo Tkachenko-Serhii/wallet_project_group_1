59a5f692eb46dedcfa3156696c5ae79c1806b439
Igor Griniov <griniov.igor@gmail.com>
2022-02-23 13:42:45 +0200

    cool date

--- src/components/DashboardItem/DashboardItem.jsx
+++ src/components/DashboardItem/DashboardItem.jsx
@@ -1,15 +1,16 @@
 import s from './DashboardItem.module.css';
 
 export default function DashboardItem({ row }) {
-  // const splitedDate = row.date.split('.');
-  // const cuted = splitedDate[2].substr(-2);
-  // const formatedDate = splitedDate[0] + '.' + splitedDate[1] + '.' + cuted;
+  const dateToFormat = new Date(row.date);
+  const splitedDate = dateToFormat.toLocaleDateString().split('.');
+  const cuttedYear = splitedDate[2].substr(-2);
+  const formatedDate = splitedDate[0] + '.' + splitedDate[1] + '.' + cuttedYear;
   const formatedSum = row.sum / 100;
   const formatedBalance = row.balance / 100;
   return (
     <li className={s.transactionRowWrapper}>
       <ul className={s.transactionRow}>
-        <li className={s.transactionCellDate}>{row.dat}</li>
+        <li className={s.transactionCellDate}>{formatedDate}</li>
         <li className={s.transactionCellType}>{row.type ? '+' : '-'}</li>
         <li className={s.transactionCellCategory}>{row.category}</li>
         <li className={s.transactionCellComment}>{row.comment}</li>
@@ -17,10 +18,10 @@ export default function DashboardItem({ row }) {
           className={s.transactionCellSum}
           style={row.type ? { color: 'green' } : { color: 'red' }}
         >
-          {formatedSum.toLocaleString().replace(/,/i, '.')}
+          {formatedSum.toFixed(2).toLocaleString().replace(/,/i, '.')}
         </li>
         <li className={s.transactionCellBalance}>
-          {formatedBalance.toLocaleString().replace(/,/i, '.')}
+          {formatedBalance.toFixed(2).toLocaleString().replace(/,/i, '.')}
         </li>
       </ul>
     </li>
